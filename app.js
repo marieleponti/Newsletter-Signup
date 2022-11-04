31900e02b96130f157677b542ea21de3050154f5
Mariele Ponticiello <mponticiello@beltrailway.com>
2022-11-04 16:50:29 -0500

    added get and post requests

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const { urlencoded } = require("body-parser");
+const https = require("https");
+const { post } = require("request");
+
+const app = express();
+const apiKey = "13b5b0ad23b62ebdf6fc9c042475711e-us17";
+const listId = "d9ed8c2cb8";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({extended:true}));
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000.")
+});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+   
+    var jsonData = JSON.stringify(data);
+
+    const url = "https://us17.api.mailchimp.com/3.0/lists/" + listId;
+
+    const options = {
+        method: "POST",
+        auth: "mp:" + apiKey
+    }
+
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            if (response.statusCode == 200){
+                res.sendFile(__dirname + '/success.html');
+            } else {
+                res.sendFile(__dirname + "failure.html");
+            }
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+   
+});
+
